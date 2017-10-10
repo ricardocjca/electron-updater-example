97801d733826ffd5f7af75b0318f00f71ac4d93a
Ricardo Braga <ricardocjca@gmail.com>
2017-10-10 17:59:50 -0400

    mudanca na leitura download

--- main.js
+++ main.js
@@ -80,9 +80,20 @@ autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error in auto-updater.');
 })
 autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  // let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  // log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  // log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  // sendStatusToWindow(log_message);
+
+
+  let speed = ((progressObj.bytesPerSecond / 1000) / 1000).toFixed(1);
+  let transferred = ((progressObj.transferred / 1000) / 1000).toFixed(1);
+  let total = ((progressObj.total / 1000) / 1000).toFixed(1);
+  let percent = progressObj.percent.toFixed(1);
+
+  let log_message = "Download speed: " + speed + " Mb/s";
+  log_message = log_message + ' - Downloaded: ' + percent + '%';
+  log_message = log_message + ' (' + transferred + "/" + total + ')';
   sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
