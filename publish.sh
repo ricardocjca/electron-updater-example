302aeacaaaee21343678c3dc350b40857f01cfed
Ricardo Braga <ricardocjca@gmail.com>
2017-10-20 01:04:41 -0400

    testatte

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-if [ -z "26368d1018cb8b98a82a3b6adfebc16fd6e25bcc" ]; then
+export GH_TOKEN="364a00d85f0dbf998e2c4e5f18471ec149930329"
+if [ -z "$GH_TOKEN" ]; then
     echo "You must set the GH_TOKEN environment variable."
     echo "See README.md for more details."
     exit 1
